499dd4658bfb34269bd1fd69e488d5e0b69afc70
thesportydar <ipaladinobravo@gmail.com>
2025-02-15 23:13:13 -0300

    Acortador de URLs

--- src/i18n/projects.ts
+++ src/i18n/projects.ts
@@ -18,12 +18,20 @@ export const projectsTranslations = {
                 "image": "estim-demo.png"
             },
             {
-                "name": "API REST",
+                "name": "Ecommerce API REST",
                 "description": "<p><strong>API REST</strong> diseñada para gestionar el flujo completo de un sistema de compras en línea. Permite a los usuarios registrarse, autenticarse y explorar productos, mientras ofrece funcionalidades para agregar y actualizar artículos en un carrito de compras y procesar pedidos. La API sigue principios de <strong>arquitectura limpia</strong>, con una clara separación de responsabilidades y documentación detallada, lo que facilita su integración con aplicaciones front-end o móviles.</p>",
                 "tags": ["Spring Boot", "Spring Security", "Spring Data JPA", "JJWT", "PostgreSQL", "Swagger"],
                 "github": "https://www.github.com/thesportydar/shopping-cart",
                 "live": "https://thesportydar.github.io/shopping-cart-api-docs",
                 "image": "api-rest-demo.png"
+            },
+            {
+                "name": "Acortador de URLs",
+                "description": "<p>Aplicación web para acortar URLs de forma sencilla y rápida. Permite a los usuarios acortar enlaces largos y personalizar la URL corta. La aplicación fue desarrollada con <strong>Spring Boot</strong> y <strong>PostgreSQL</strong> en el backend, y <strong>Astro</strong> y <strong>Preact</strong> en el frontend.</p>",
+                "tags": ["Spring Boot", "PostgreSQL", "Astro", "Preact", "TailwindCSS"],
+                "github": "https://www.github.com/thesportydar/url-shortener-backend",
+                "live": "http://link.inaqui.me/",
+                "image": "shortener.png"
             },
             {
                 "name": "Compilador",
@@ -67,7 +75,7 @@ export const projectsTranslations = {
                 "image": "estim-demo.png"
             },
             {
-                "name": "API REST",
+                "name": "Ecommerce API REST",
                 "description": "<p><strong>REST API</strong> designed to manage the complete flow of an online shopping system. Allows users to register, authenticate and explore products, while offering functionalities to add and update items in a shopping cart and process orders. The API follows <strong>clean architecture</strong> principles, with a clear separation of responsibilities and detailed documentation, making it easy to integrate with front-end or mobile applications.</p>",
                 "tags": ["Spring Boot", "Spring Security", "Spring Data JPA", "JJWT", "PostgreSQL", "Swagger"],
                 "github": "https://www.github.com/thesportydar/shopping-cart",
@@ -75,14 +83,22 @@ export const projectsTranslations = {
                 "image": "api-rest-demo.png"
             },
             {
-                "name": "Compilador",
+                "name": "URL Shortener",
+                "description": "<p>Web application to shorten URLs quickly and easily. Allows users to shorten long links and customize the short URL. The application was developed with <strong>Spring Boot</strong> and <strong>PostgreSQL</strong> in the backend, and <strong>Astro</strong> and <strong>Preact</strong> in the frontend.</p>",
+                "tags": ["Spring Boot", "PostgreSQL", "Astro", "Preact", "TailwindCSS"],
+                "github": "https://www.github.com/thesportydar/url-shortener-backend",
+                "live": "http://link.inaqui.me/",
+                "image": "shortener.png"
+            },
+            {
+                "name": "Compiler",
                 "description": "<p>Project developed as part of the Compiler Design 1 2023 course. It consists of a compiler for a custom programming language. The compiler was developed in <strong>Java</strong> using the <strong>YACC</strong> tool, and the output code is assembler for the <strong>x86 architecture (pentium 32 bits)</strong>.</p>",
                 "tags": ["Java", "YACC", "Assembly"],
                 "github": "https://www.github.com/thesportydar/compilador",
                 "image": "compiler-demo.png"
             },
             {
-                "name": "Corrector Ortográfico",
+                "name": "Spell Checker",
                 "description": "<p>This project was created as part of the final work of the Java programming workshop. It consists of the implementation of a <strong>Java spell checker</strong>, which covers various functionalities for text processing, error detection and suggestions of corrections. Implements 4 types of correctors: Based on dictionary of corrections, based on adjacent permutations, and two of <strong>Levenshtein</strong>, one based on map and another on <strong>trie</strong>.</p>",
                 "tags": ["Java", "Trie", "Levenshtein", "Junit", "Benchmark", "JMH"],
                 "github": "https://www.github.com/thesportydar/spellchecker",
