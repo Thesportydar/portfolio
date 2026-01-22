5f70f9cca0669fa52c1b9f6fd38067df09d3fabd
Thesportydar <ipaladinobravo@gmail.com>
2026-01-22 15:13:50 -0300

    update tech stack, clean projects & align welcome msg

--- src/i18n/projects.ts
+++ src/i18n/projects.ts
@@ -30,49 +30,11 @@ export const projectsTranslations = {
                 "image": "infra-demo.webp"
             },
             {
-                "name": "Estim Store",
-                "description": "<p>Aplicación full-stack construida con el <strong>stack MEAN</strong> que presenta un extenso catálogo de más de 27,000 videojuegos basado en un dataset de juegos de Steam. La plataforma permite a los usuarios explorar, buscar y filtrar títulos con información detallada sobre cada juego, ofreciendo una experiencia interactiva e intuitiva.</p>",
-                "tags": ["AngularJS", "Node.js", "Express", "MongoDB", "Mongoose"],
-                "github": "https://www.github.com/thesportydar/estim/tree/dev",
-                "live": "https://estim.onrender.com",
-                "image": "estim-demo.webp"
-            },
-            {
-                "name": "API REST",
-                "description": "<p><strong>API REST</strong> diseñada para gestionar el flujo completo de un sistema de compras en línea. Permite a los usuarios registrarse, autenticarse y explorar productos, mientras ofrece funcionalidades para agregar y actualizar artículos en un carrito de compras y procesar pedidos. La API sigue principios de <strong>arquitectura limpia</strong>, con una clara separación de responsabilidades y documentación detallada, lo que facilita su integración con aplicaciones front-end o móviles.</p>",
-                "tags": ["Spring Boot", "Spring Security", "Spring Data JPA", "JJWT", "PostgreSQL", "Swagger"],
-                "github": "https://www.github.com/thesportydar/shopping-cart",
-                "live": "https://thesportydar.github.io/shopping-cart-api-docs",
-                "image": "api-rest-demo.webp"
-            },
-            {
-                "name": "Acortador de URLs",
-                "description": "<p>Aplicación web para acortar URLs de forma sencilla y rápida. Permite a los usuarios acortar enlaces largos y personalizar la URL corta. La aplicación fue desarrollada con <strong>Spring Boot</strong> y <strong>PostgreSQL</strong> en el backend, y <strong>Astro</strong> y <strong>Preact</strong> en el frontend. Ademas cuenta con un <strong>sistema de caching</strong> a traves de <strong>Redis</strong> para mejorar la velocidad de acceso a las URLs acortadas.</p>",
-                "tags": ["Spring Boot", "PostgreSQL", "Astro", "Preact", "TailwindCSS", "Redis"],
-                "github": "https://www.github.com/thesportydar/url-shortener-backend",
-                "live": "https://link.inaqui.me/",
-                "image": "shortener.webp"
-            },
-            {
                 "name": "Compilador",
                 "description": "<p>Proyecto desarrollado como parte de la cursada de Diseño de Compiladores 1 2023. Consiste en un compilador de un lenguaje de programación propio. El compilador fue desarrollado en <strong>Java</strong> utilizando la herramienta <strong>YACC</strong>, y el codigo de salida es ensamblador para la arquitectura <strong>x86 (pentium 32 bits)</strong>.</p>",
                 "tags": ["Java", "YACC", "Assembly"],
                 "github": "https://www.github.com/thesportydar/compilador",
                 "image": "compiler-demo.webp"
-            },
-            {
-                "name": "Corrector Ortográfico",
-                "description": "<p>Este proyecto se creo en el marco del trabajo final del taller de programacion en Java. Consiste en la implementación de un <strong>corrector ortográfico en Java</strong>, que abarca diversas funcionalidades para el procesamiento de texto, la detección de errores y la sugerencia de correcciones. Implementa 4 tipos de correctores: Basado en Diccionario de correciones, basado en permutaciones adyacentes, y dos de <strong>Levenshtein</strong>, uno basado en map y otro en <strong>trie</strong>.</p>",
-                "tags": ["Java", "Trie", "Levenshtein", "Junit", "Benchmark", "JMH"],
-                "github": "https://www.github.com/thesportydar/spellchecker",
-                "image": "spellchecker.webp"
-            },
-            {
-                "name": "MRI Anomaly Detector",
-                "description": "<p>Detector de tumores cerebrales en imagenes de resonancias mageneticas a partir de <strong>autoencoders</strong> usando <strong>aprendizaje no supervisado</strong>. Se utilizo una arquitectura sencialla para enfocarse en el entendimiento del proceso de detección de anomalias a partir de autoencoders. No se usaron bloques residuales ni técnicas de regularización como dropout o batch normalization.</p>",
-                "tags": ["Python", "TensorFlow", "Keras", "Colab"],
-                "live": "https://colab.research.google.com/drive/1m856pNYCyWsdoSDelg4UDoTKufmgVGNL?usp=sharing",
-                "image": "autoencoder.webp"
             }
         ]
     },
@@ -107,49 +69,11 @@ export const projectsTranslations = {
                 "image": "infra-demo.webp"
             },
             {
-                "name": "Estim Store",
-                "description": "<p>Full-stack application built with the <strong>MEAN stack</strong> that features an extensive catalog of over 27,000 video games based on a dataset of Steam games. The platform allows users to explore, search and filter titles with detailed information about each game, offering an interactive and intuitive experience.</p>",
-                "tags": ["AngularJS", "Node.js", "Express", "MongoDB", "Mongoose"],
-                "github": "https://www.github.com/thesportydar/estim/tree/dev",
-                "live": "https://estim.onrender.com",
-                "image": "estim-demo.webp"
-            },
-            {
-                "name": "API REST",
-                "description": "<p><strong>REST API</strong> designed to manage the complete flow of an online shopping system. Allows users to register, authenticate and explore products, while offering functionalities to add and update items in a shopping cart and process orders. The API follows <strong>clean architecture</strong> principles, with a clear separation of responsibilities and detailed documentation, making it easy to integrate with front-end or mobile applications.</p>",
-                "tags": ["Spring Boot", "Spring Security", "Spring Data JPA", "JJWT", "PostgreSQL", "Swagger"],
-                "github": "https://www.github.com/thesportydar/shopping-cart",
-                "live": "https://thesportydar.github.io/shopping-cart-api-docs",
-                "image": "api-rest-demo.webp"
-            },
-            {
-                "name": "URL Shortener",
-                "description": "<p>Web application to shorten URLs quickly and easily. Allows users to shorten long links and customize the short URL. The application was developed with <strong>Spring Boot</strong> and <strong>PostgreSQL</strong> in the backend, and <strong>Astro</strong> and <strong>Preact</strong> in the frontend. It also has a <strong>caching system</strong> through <strong>Redis</strong> to improve the speed of access to shortened URLs.</p>",
-                "tags": ["Spring Boot", "PostgreSQL", "Astro", "Preact", "TailwindCSS", "Redis"],
-                "github": "https://www.github.com/thesportydar/url-shortener-backend",
-                "live": "https://link.inaqui.me/",
-                "image": "shortener.webp"
-            },
-            {
                 "name": "Compiler",
                 "description": "<p>Project developed as part of the Compiler Design 1 2023 course. It consists of a compiler for a custom programming language. The compiler was developed in <strong>Java</strong> using the <strong>YACC</strong> tool, and the output code is assembler for the <strong>x86 architecture (pentium 32 bits)</strong>.</p>",
                 "tags": ["Java", "YACC", "Assembly"],
                 "github": "https://www.github.com/thesportydar/compilador",
                 "image": "compiler-demo.webp"
-            },
-            {
-                "name": "Spell Checker",
-                "description": "<p>This project was created as part of the final work of the Java programming workshop. It consists of the implementation of a <strong>Java spell checker</strong>, which covers various functionalities for text processing, error detection and suggestions of corrections. Implements 4 types of correctors: Based on dictionary of corrections, based on adjacent permutations, and two of <strong>Levenshtein</strong>, one based on map and another on <strong>trie</strong>.</p>",
-                "tags": ["Java", "Trie", "Levenshtein", "Junit", "Benchmark", "JMH"],
-                "github": "https://www.github.com/thesportydar/spellchecker",
-                "image": "spellchecker.webp"
-            },
-            {
-                "name": "MRI Anomaly Detector",
-                "description": "<p>Brain tumor detector in magnetic resonance images using <strong>autoencoders</strong> with <strong>unsupervised learning</strong>. A simple architecture was used to focus on understanding the anomaly detection process from autoencoders. No residual blocks or regularization techniques such as dropout or batch normalization were used.</p>",
-                "tags": ["Python", "TensorFlow", "Keras", "Colab"],
-                "live": "https://colab.research.google.com/drive/1m856pNYCyWsdoSDelg4UDoTKufmgVGNL?usp=sharing",
-                "image": "autoencoder.webp"
             }
         ]
     }
